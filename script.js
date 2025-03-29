a54cab090c8c09b2af8f2d60b19a7685a7bdcbf8
Deion Menor <deion@dorxata.com>
2025-03-29 18:59:05 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// STARTER CODE
+
+// 1. Get references to the DOM elements we need
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+// 2. Add event listeners
+
+// 3. Function to add a new task
+
+// 4. Function to mark a task as complete
+
